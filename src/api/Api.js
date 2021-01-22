10cfbe21e4df99c4907d8faf1694bfa3dcb1a1d3
Alexey Ilyin <angrymarshall@gmail.com>
2021-01-22 22:28:22 +0300

    пробуем пробуем пробуем

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,16 +1,13 @@
 import axios from "axios";
 import React from "react";
 
-const instance = axios.create({
-    baseURL: 'http://localhost:3001/',
-});
-
 export const pizzaApi = {
     getPizzas(sortBy, category) {
-        return instance
-            .get(`pizzas?${
-                category !== null ? `category=${category}` : ''
-            }&_sort=${sortBy.type}&_order=${sortBy.order}`)
-            .catch((err) => {console.log(err)});
+        return axios
+            .get(
+                `/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortBy.type}&_order=${
+                    sortBy.order
+                }`,
+            )
     }
 }
